285e3704ad19b9ca14ecefde94b256f33825f41c
Bruno Marra <bcm.marra@gmail.com>
2025-12-30 16:21:01 -0300

    sql

--- sql.sql
+++ sql.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS controle_financeiro;
 USE controle_financeiro;
 
 -- 2. Tabela de Usuários
-CREATE TABLE usuarios (
+CREATE TABLE  IF NOT EXISTS usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
@@ -11,13 +11,13 @@ CREATE TABLE usuarios (
 );
 
 -- 3. Tabela de Categorias
-CREATE TABLE categorias (
+CREATE TABLE IF NOT EXISTS categorias (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL
 );
 
 -- 4. Tabela de Transações
-CREATE TABLE transacoes (
+CREATE TABLE IF NOT EXISTS transacoes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT NOT NULL,
     categoria_id INT NOT NULL,
@@ -35,27 +35,17 @@ CREATE TABLE transacoes (
 );
 
 -- 5. Inserindo algumas categorias iniciais para teste
-INSERT INTO categorias (nome)
+INSERT IGNORE INTO categorias (nome)
 VALUES 	('Alimentação'),
 		('Transporte'),
 		('Lazer'),
         ('Salário'),
         ('Aluguel');
 
-SELECT * FROM categorias;
-SELECT * FROM transacoes;
-SELECT * FROM usuarios;
-
-USE controle_financeiro;
-
 -- Criando o primeiro usuário para teste
-INSERT INTO usuarios (nome, email, senha) 
+INSERT IGNORE INTO usuarios (nome, email, senha) 
 VALUES ('Usuario Teste', 'bruno@email.com', '123456');
 
-
-USE controle_financeiro;
-
--- Garante que o usuário ID 1 existe
 INSERT IGNORE INTO usuarios (id, nome, email, senha) 
 VALUES (1, 'Bruno Marra', 'bruno@email.com', '123456');
 
@@ -63,3 +53,26 @@ VALUES (1, 'Bruno Marra', 'bruno@email.com', '123456');
 INSERT IGNORE INTO categorias (id, nome) 
 VALUES (1, 'Geral');
 
+UPDATE categorias SET nome = TRIM(nome);
+ALTER TABLE categorias ADD CONSTRAINT uc_nome_categoria UNIQUE (nome);
+
+-- SET SQL_SAFE_UPDATES = 0;
+-- UPDATE categorias SET nome = TRIM(nome);
+-- SET SQL_SAFE_UPDATES = 1;
+
+SELECT * FROM categorias;
+SELECT * FROM transacoes;
+SELECT * FROM usuarios;
+
+
+SET SQL_SAFE_UPDATES = 0;
+
+DELETE c1 FROM categorias c1
+INNER JOIN categorias c2 
+WHERE c1.id > c2.id AND c1.nome = c2.nome;
+
+SET SQL_SAFE_UPDATES = 1;
+ALTER TABLE categorias ADD CONSTRAINT uc_nome_categoria UNIQUE (nome);
+
+ALTER TABLE transacoes ADD COLUMN pago BOOLEAN DEFAULT FALSE;
+ALTER TABLE transacoes ADD COLUMN metodo_pagamento VARCHAR(50);
